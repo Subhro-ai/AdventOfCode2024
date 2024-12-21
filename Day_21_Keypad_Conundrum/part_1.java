0779f5b265559b3fd440727d6679529ecd0de657
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-12-21 22:45:04 +0530

    Day 21 Keypad Conundrum Keypad 1 Working

---
+++ Day_21_Keypad_Conundrum/part_1.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class part_1 {
+    static String keypad[][] = { { "7", "8", "9" }, { "4", "5", "6" }, { "1", "2", "3" }, { "null", "0", "A" } };
+    
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            ArrayList<String> path = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                path = keypadPath(line);
+                System.out.println(line);
+                tSystem.out.println(path);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> keypadPath(String line) {
+        ArrayList<String> path = new ArrayList<>();
+        String init = "A";
+        String fin = "A";
+        // System.out.println(line);
+        for (int k = 0; k < line.length(); k++) {
+            init = fin;
+            fin = String.valueOf(line.charAt(k));
+            // System.out.println("From " + init + " To " + fin);
+
+            int[] initArr = getIndex(init);
+            int[] finArr = getIndex(fin);
+
+            // If either init or fin is not found, do not proceed further
+            if (initArr == null || finArr == null) {
+                // System.out.println("Invalid input: " + init + " or " + fin + " not found.");
+                return null;
+            }
+
+            int[] move = { (initArr[0] - finArr[0]), (initArr[1] - finArr[1]) };
+            // System.out.println(move[0] + "" + move[1]);
+
+            // Handle movement in horizontal direction (left-right)
+            for (int i = 0; i < Math.abs(move[0]); i++) {
+                if (move[0] > 0) {
+                    path.add(">");
+                } else {
+                    path.add("<");
+                }
+            }
+            for (int j = 0; j < Math.abs(move[0]); j++) {
+                if (move[0] > 0) {
+                    path.add("^");
+                } else {
+                    path.add("v");
+                }
+            }
+            path.add("A");
+        }
+        return path;
+    }
+
+    public static int[] getIndex(String a) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (keypad[i][j] != null && keypad[i][j].equals(a)) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null; // Return null if element is not found
+    }
+}
